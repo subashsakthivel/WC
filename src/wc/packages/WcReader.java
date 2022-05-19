18ffee76afa773db667efc13bb9b1c473b0ff05e
Subash Sakthivel S <subashsakthi01@gmail.com>
2022-05-19 19:09:01 +0530

    updated

--- src/wc/packages/WcReader.java
+++ src/wc/packages/WcReader.java
@@ -1,135 +1,167 @@
 package wc.packages;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
-public class WcReader implements WcOperations, Runnable {
-    public static volatile int totalLine = 0;
-    public static volatile int totalChars = 0;
-    public static volatile int totalWords = 0;
-    public static volatile int totalBytes = 0;
-    public File file = null;
-    public FileInputStream fis = null;
-    private boolean fileRead = false;
-    public BufferedInputStream bufferedInputStream = null;
-    private int noLines = 0;
-    private int noWords = 0;
-    private int noChars = 0;
-    private int noBytes = 0;
-    private int longLine = 0;
+ class WcReader implements Runnable{
+     static private class Totals{
+         public static long nBytes;
+         public static long nChars;
+         public static long nWords;
+         public static long nLines;
+     }
 
-    public  WcReader(String  path) throws IOException {
-        file = new File(path);
-        fis = new FileInputStream(file);
-        bufferedInputStream = new BufferedInputStream(fis);
+    private final FileChannel fileChannel;
+    private final String fileName;
+    private final boolean large;
+    private final long limit;
+    private final long nBytes;
+    private final long nChars;
+    private long nWords;
+    private long nLines;
+
+
+    public WcReader(String fileName , FileChannel fileChannel , long start, long limit, boolean large) throws IOException {
+        this.fileName = fileName;
+        this.fileChannel = fileChannel;
+        this.nChars = limit-start;
+        this.nBytes = nChars*Character.BYTES;
+        this.limit = limit;
+        this.large = large;
+        if(large) {
+            if (start > 0) fileChannel.position(start - 1);
+            if (start == 0) nWords++;
+            else fileChannel.position(start);
+        }
     }
 
-    public WcReader() {
-
-    }
-
-    private void reader() {
-        int read;
-        int lineChars =0;
-        int longLineChars = 0;
-        try {
-            if (this.noLines == 0 || this.noWords == 0 || this.noChars == 0) {
-                while ((read = bufferedInputStream.read()) > -1) {
-                    this.noChars++;
-                    lineChars++;
-                    if ((char) read == ' ') {
-                        this.noWords++;
-                    } else if ((char) read == '\n') {
-                        this.noLines++;
-                        this.noWords++;
-                        if (lineChars > longLineChars) {
-                            longLineChars = lineChars;
-                            this.longLine = this.noLines;
-                        }
-                        lineChars = 0;
-                    }
-                }
-            }
-            if(this.noChars > 0)
-            {
-                this.noLines++;
-                this.noWords++;
-                if(lineChars > longLineChars)
+    private void reader() throws IOException{
+        int noOfBytesRead = 0;
+        while(noOfBytesRead != -1) {
+            ByteBuffer buffer = ByteBuffer.allocate(10000);
+            noOfBytesRead = fileChannel.read(buffer);
+            buffer.flip();
+            char x;
+            while(buffer.hasRemaining() ){
+                x = (char)buffer.get();
+                if(x=='\n')
                 {
-                    this.longLine = this.noLines;
+                    nLines++;
+                }
+                else if(x!= ' ') {
+                    while(buffer.hasRemaining() && x!=' '){
+                        //  System.out.print(x);
+                        x = (char)buffer.get();
+                        if(x=='\n') {
+                            nLines++;
+                            break;
+                        }
+                    }
+                    nWords++;
+                    //      System.out.println(",");
                 }
             }
-            this.noBytes = this.noChars * Character.BYTES;
-        } catch ( Exception e) {
-            e.printStackTrace();
+        }
+        synchronized (this) {
+            Totals.nBytes += this.nBytes;
+            Totals.nChars += this.nChars;
+            Totals.nLines += this.nLines;
+            Totals.nWords += this.nWords;
+        }
+    }
+    private void readLarger() throws IOException {
+        int noOfBytesRead = 0;
+        long start= fileChannel.position();
+        boolean precaution = true;
+        while(noOfBytesRead != -1 && start<limit) {
+            ByteBuffer buffer = ByteBuffer.allocate(10000);
+            noOfBytesRead = fileChannel.read(buffer);
+            buffer.flip();
+            char x;
+            if(precaution && buffer.hasRemaining()) {
+                x = (char) buffer.get();
+                if (x != ' ') nWords--;
+                if (x == '\n') nWords++;
+                precaution = false;
+            }
+            while(buffer.hasRemaining() && start<limit){
+                x = (char)buffer.get();
+                start++;
+                if(x=='\n')
+                {
+                    nLines++;
+                }
+                else if(x!= ' ') {
+                    while(buffer.hasRemaining() && x!=' ' && start<limit){
+                      //  System.out.print(x);
+                        x = (char)buffer.get();
+                        start++;
+                        if(x=='\n') {
+                            nLines++;
+                            break;
+                        }
+                    }
+                    nWords++;
+              //      System.out.println(",");
+                }
+            }
+        }
+        synchronized (this) {
+            Totals.nBytes += this.nBytes;
+            Totals.nChars += this.nChars;
+            Totals.nLines += this.nLines;
+            Totals.nWords += this.nWords;
         }
     }
 
-    @Override
-    public String toString() {
-        reader();
-        try {
-            bufferedInputStream.close();
-            fis.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return file.getName() + "  "+ noLines + " " + noBytes + " " + noChars + " " + noWords;
-    }
-    public String toStrings() {
-        reader();
-        totalLine = totalLine + noLines;
-        totalWords = totalWords + noWords;
-        totalBytes= totalBytes + noBytes;
-        totalChars= totalChars + noChars;
-        try {
-            bufferedInputStream.close();
-            fis.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return file.getName() + "  "+ noLines + " " + noBytes + " " + noChars + " " + noWords;
-    }
-    public void totals() {
-        System.out.println("Totals " + totalLine + " " + totalBytes + " " + totalChars + " " + totalWords);
+    public static void totalResult(){
+        System.out.println("-Totals : " + " " + Totals.nBytes + " " + Totals.nChars + " "+Totals.nWords +" " + Totals.nLines);
     }
 
-    @Override
-    public String lines() {
-        reader();
-        return file.getName() + " " +noLines;
+    public static void LargerResult(){
+        System.out.println(Totals.nBytes + " " + Totals.nChars + " "+Totals.nWords + " "+ (Totals.nLines+1));
     }
 
-    @Override
-    public String words() {
-        reader();
-        return file.getName() + " " +noWords;
+    private void result(){
+       // "Bytes Chars Words Lines"
+        System.out.println("-" + fileName + " " + nBytes + " " + nChars + " "+nWords + " "+ nLines );
     }
-
-    @Override
-    public String chars() {
-        reader();
-        return file.getName() + " :" +noChars;
-    }
-
-    @Override
-    public String bytes() {
-        reader();
-        return file.getName() + " " +noBytes*Character.BYTES;
-    }
-
-    @Override
-    public String longLineLen() {
-        reader();
-        return file.getName() + " " +longLine;
-    }
-
 
     @Override
     public void run() {
-        System.out.println(this);
-
+        try {
+            if(large) {
+                readLarger();
+            } else {
+                reader();
+                result();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-}
+
+    protected long lines() throws IOException {
+        reader();
+         return this.nLines;
+     }
+
+     protected long words() throws IOException {
+        reader();
+         return this.nWords;
+     }
+
+     protected long chars() {
+         return this.nChars;
+     }
+
+     protected long bytes() {
+         return this.nBytes;
+     }
+
+     protected String details() throws IOException {
+        reader();
+        return nBytes + " " + nChars + " "+nWords + " "+ nLines;
+     }
+ }
